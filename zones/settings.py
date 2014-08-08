45fa3790beef5a1d8c7cdabaa3d9271608951922
jivlev <jenia.ivlev@gmail.com>
2014-08-08 19:55:42 -0400

    Entered a default secret key.

--- zones/settings.py
+++ zones/settings.py
@@ -103,7 +103,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = ''
+SECRET_KEY = '1234'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
