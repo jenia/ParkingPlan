42d45de8a1aa1496d82c0631e23de49bc04ab30c
jivlev <jenia.ivlev@gmail.com>
2014-08-08 19:56:22 -0400

    changed username in settings.py to something "yourname"

--- zones/settings.py
+++ zones/settings.py
@@ -33,7 +33,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'map',                      # Or path to database file if using sqlite3.
-        'USER': 'jenia',                      # Not used with sqlite3.
+        'USER': 'yourname',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
